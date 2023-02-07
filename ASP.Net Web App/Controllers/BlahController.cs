dabb866b1dc0154b85e308c459fa67956ad5c13d
nati2024 <95377889+nati2024@users.noreply.github.com>
2023-02-06 21:29:23 -0700

    mission 05

---
+++ ASP.Net Web App/Controllers/BlahController.cs
@@ -0,0 +1,29 @@
+﻿using ASP.Net_Web_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.Net_Web_App.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult OtherView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult OtherView(Calc model)
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.Net Web App/Models/Calc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.Net_Web_App.Models
+{
+    public class Calc
+    {
+        [Range(0,100, ErrorMessage = "Number needs to be between 0 and 100")]
+        public int assignments { get; set; }
+        [Range(0, 100, ErrorMessage = "Number needs to be between 0 and 100")]
+        public int project { get; set; }
+        [Range(0, 100, ErrorMessage = "Number needs to be between 0 and 100")]
+        public int quiz { get; set; }
+        [Range(0, 100, ErrorMessage = "Number needs to be between 0 and 100")]
+        public int midterm { get; set; }
+        [Range(0, 100, ErrorMessage = "Number needs to be between 0 and 100")]
+        public int final { get; set; }
+        [Range(0, 100, ErrorMessage = "Number needs to be between 0 and 100")]
+        public int intex { get; set; }
+       
+    }
+}
